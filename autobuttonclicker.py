e62fa3325bed6e5afc700d388df2bf85d66a6978
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-05-27 02:54:48 -0500

    Update autobuttonclicker.py

--- autobuttonclicker.py
+++ autobuttonclicker.py
@@ -3,12 +3,6 @@ import time
 import keyboard
 
 counter = 0
-def click_button(button):
-    pyautogui.click(button.left + button.width // 2, button.top + button.height // 2)
-    global counter
-    counter += 1
-    print(f"Button clicked ({counter} times)")
-    time.sleep(1.5)
 
 while True:
     if keyboard.is_pressed('esc'):
@@ -16,13 +10,13 @@ while True:
     try:
         found_button1 = pyautogui.locateOnScreen('button.png')
         if found_button1:
-            click_button(found_button1)
-
-        found_button2 = pyautogui.locateOnScreen('graybutton.png')
-        if found_button2:
-            click_button(found_button2)
+            pyautogui.click(button.left + button.width // 2, button.top + button.height // 2)
+            counter += 1
+            print(f"Button clicked ({counter} times)")
+            time.sleep(1.5)
 
     except pyautogui.ImageNotFoundException:
+        pyautogui.moveRel(-100, 0)
         print("Image not found. Retrying in 1 second...")
         time.sleep(1)
 
